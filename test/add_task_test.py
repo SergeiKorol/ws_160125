5eab6d61576c0511ae18336f0466b22a12bf1a36
NKosteg <kosteg.n@yandex.ru>
2025-01-16 21:13:52 +0300

    First branch, add

--- test/add_task_test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def test_add():
-    body = {"title":"generated","completed":False}
-    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
-    response_body = response.json()
-    
-    assert response.status_code == 200
